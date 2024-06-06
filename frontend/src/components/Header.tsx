f98662b275f4b7a90080b5ee42deab86d3aff060
vikalp-tomer <vikalptomar15@gmail.com>
2024-06-06 22:25:33 +0530

    added Hero section

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="bg-blue-800 py-6 ">
-      <div className="container mx-auto flex justify-between items-center">
-        <span className="text-3xl  px-3 text-white font-bold tracking-tighter">
-          Merneats.com
+      <div className="container mx-auto flex justify-between">
+        <span className="text-3xl text-white font-bold tracking-tighter">
+          <Link to={"/"}>Merneats.com</Link>
         </span>
         <span>
           <Link
             to={"/sign-in"}
-            className="text-blue-600 bg-white px-2 py-1 font-bold hover:bg-gray-100"
+            className="text-blue-600 bg-white px-2 flex items-center font-bold hover:bg-gray-100"
           >
             Sign In
           </Link>
