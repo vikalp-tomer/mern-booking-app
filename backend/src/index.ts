291dd7d22968a1582807cdced9091d6280c02969
vikalp-tomer <vikalptomar15@gmail.com>
2024-06-09 15:53:13 +0530

    add user login

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose';
 import userRoutes from './routes/users'
+import authRoutes from './routes/auth'
 
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
@@ -17,7 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes);
+
 
 app.listen(7000, () => {
     console.log('Server started on port 7000');
