39e4e82c3ab8de07bce4623c5d0e9b2f042ffa31
vikalp-tomer <vikalptomar15@gmail.com>
2024-06-20 17:28:21 +0530

    added add hotel api endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,7 @@ import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
 import path from "node:path";
 import { v2 as cloudinary } from "cloudinary";
+import myHotelRoutes from "./routes/my-hotels";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -39,6 +40,7 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/my-hotels", myHotelRoutes);
 
 app.listen(7000, () => {
   console.log("Server started on port 7000");
