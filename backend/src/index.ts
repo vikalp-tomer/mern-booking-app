d0334e6101dc98a3ad23242322e436b3238838df
vikalp-tomer <vikalptomar15@gmail.com>
2024-06-20 08:29:44 +0530

    added some deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,32 +1,38 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import "dotenv/config";
-import mongoose from 'mongoose';
-import userRoutes from './routes/users'
-import authRoutes from './routes/auth'
-import cookieParser from 'cookie-parser'
+import mongoose from "mongoose";
+import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
+import cookieParser from "cookie-parser";
+import path from "node:path";
 
-
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((err) => {
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-}));
+  })
+);
 
-app.use("/api/auth", authRoutes)
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
-
 app.listen(7000, () => {
-    console.log('Server started on port 7000');
+  console.log("Server started on port 7000");
 });
